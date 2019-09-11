c22323cced3c441166a335972d89f351a39db8be
Eric Lorenzana <eric.lorenzana@protonmail.ch>
2019-09-11 18:52:32 +0200

    Add first implementation of search-distributions

--- quicklisp-capi.lisp
+++ quicklisp-capi.lisp
@@ -1,3 +1,11 @@
 ;;;; quicklisp-capi.lisp
 
 (in-package #:quicklisp-capi)
+
+(defun search-distributions (term)
+  "From the given query string, return a list of distributions."
+  (setf term (string-downcase term))
+  (loop for system in (ql-dist:provided-systems t)
+        when (or (search term (ql-dist:name system))
+                 (search term (ql-dist:name (ql::release system))))
+        collect system))

f33209e76a77cb44845c88f71f235b4599c751df
Eric Lorenzana <eric.lorenzana@protonmail.ch>
2019-09-11 19:05:16 +0200

    Create main interface

--- quicklisp-capi.lisp
+++ quicklisp-capi.lisp
@@ -11,3 +11,36 @@
           when (or (search-term system)
                    (search-term (ql::release system)))
           collect system)))
+
+(capi:define-interface main-window ()
+  ()
+  (:panes
+   (text-input-search capi:text-input-pane
+                 :title "Search distribution")
+   (push-button-search capi:push-button
+                       :text "OK"
+                       :callback #'(lambda (data interface)
+                                     (declare (ignore data interface))
+                                     nil ; nyi
+                                     ))
+   (list-panel-result capi:list-panel
+                      :items '(work in progress)))
+  (:layouts
+   (layout-main capi:column-layout
+                '(layout-search
+                  layout-result))
+   (layout-search capi:row-layout
+                  '(text-input-search
+                    push-button-search)
+                  :title "Search"
+                  :title-position :frame)
+   (layout-result capi:column-layout
+                  '(list-panel-result)
+                  :title "Result"
+                  :title-position :frame))
+  (:default-initargs :title "Quicklisp"
+   :best-width 480
+   :best-height 640))
+
+(defun main ()
+  (capi:display (make-instance 'main-window)))
